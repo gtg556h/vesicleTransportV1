91fe33bdd493eb998bb8ce30140539c4d28a2e86
brian <brn.j.williams@gmail.com>
2012-06-18 15:57:18 -0500

    after meeting

--- callContinuous.m
+++ callContinuous.m
@@ -12,6 +12,9 @@ cd ~/doc/research/projects/vesicleTransport/sampleData
 load 1_03min_stretch20_pdms_200fps_extrac.mat
 cd(x)
 
+%% Define direction away from cell body, [0,2pi], 0 associated with positive x-direction
+theta0 = 0.6266;   %radians
+
 %% Add vesicleTransport library to path
 addpath ~/git/vesicleTransport
 
@@ -33,7 +36,7 @@ dy = dx;
 %buildParticleListManual;  % FUNCTION TO MANUALLY SELECT PARTICLES
 %analyze = indexVec;
 analyze = [2 6];% 7 18 6 179 15 69 204];
-analyze = [2 4 6 7 8 10 13 15 18 69 86 94 110 195 204 395 397 403 425 475 476 578 626];
+%analyze = [2 4 6 7 8 10 13 15 18 69 86 94 110 195 204 395 397 403 425 475 476 578 626];
 
 %% Declare continuout MSD parameters
 maxTau = 260E-3;
@@ -44,10 +47,16 @@ slopeMax = 200E-3;
 
 %% Calculat meanLogSlope
 for ii = analyze
+    ii
+    xPosLong{ii} = xPos{ii};
+    yPosLong{ii} = yPos{ii};
     [MSD{ii}, meanLogSlope{ii}, tau{ii}, xPos{ii}, yPos{ii}, t{ii}] = continuousMSD(xPos{ii}, yPos{ii}, maxTau, slopeMin, slopeMax, dt);
 end
 
-
+%% Calculate angle by linear fit of neighborhood
+for ii = analyze
+    theta{ii} = findAngle(xPosLong{ii},yPosLong{ii},startIndex)
+end
 
 
 %% Smooth meanLogSlope
@@ -65,16 +74,28 @@ end
 %     pause
 % end
 
-% Raw velocity calculation and estimation of runV, stagV
+
+%% Raw velocity calculation and estimation of runV, stagV
 for ii = analyze
     vx{ii} = smooth(diff(xPos{ii})/dt,30);
     vy{ii} = smooth(diff(yPos{ii})/dt,30);
     v{ii} = sqrt(vx{ii}.^2+vy{ii}.^2);
     vSmooth{ii} = v{ii};
-end    
-%%
+end  
 
-%%
+%% Determine direction. 1 -> moving away from cell body. 0 -> moving toward cell body for ii = analyze
+for ii = analyze
+    for jj = 1:length(theta{ii})
+        if abs(theta{ii}(jj)-theta0)<pi/4 | abs(theta{ii}(jj)-theta0)>3*pi/4
+            direction{ii}(jj) = 1;
+        else
+            direction{ii}(jj) = 0;
+        end
+    end
+end
+
+
+%% Find mean run velocity and mean stag velocity for entire trajectory
 for ii = analyze
     accumA = 0;
     countA = 0;
@@ -94,18 +115,31 @@ for ii = analyze
     meanStagV(ii) = accumP/countP;
 end
 
-smoothFactor = 30;
+
+%% Find and run basic processing on individual segments
+smoothFactor = 30;  %% Important! 
+
 for ii = analyze
     [event{ii},segLength{ii},segType{ii},nSeg(ii)] = segFind(meanLogSlope{ii},smoothFactor);
 end
 % Note: event is identical to ix{} in DPsimplify output
+% Note: setType = 1 implies passive motion, 2 implies undetermined, and 3
+% implies active transport
 
+
+%% Plot trajectories, color coded by transport state
 figure
 for ii = analyze
     plotBySeg(xPos{ii}, yPos{ii},event{ii},segType{ii})
     pause
+    %plotByDir(xPos{ii},yPos{ii},direction{ii})
+    %pause
     clf
 end
 
+
+
 hist(meanRunV(analyze)./meanStagV(analyze))
+pause
+close all
 
--- findAngle.m
+++ findAngle.m
@@ -6,5 +6,6 @@ diffVector = diff(xPosLong) + i*diff(yPosLong);
 
 for ii = startIndex+1:length(xPosLong)-startIndex
     theta(ii-startIndex) = angle(diffVector(ii));
+    theta(ii-startIndex) = mod(theta(ii-startIndex),2*pi);
 end
 
